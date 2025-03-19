21bf51370bf6ff54e126c79acb583fb17a65cb54
migueljnetoux <migueljneto.ux@gmail.com>
2025-03-19 22:59:21 +0000

    Ficha01_ ex01

---
+++ Ficha01_Estruturas_Condicionais/script.js
@@ -0,0 +1,22 @@
+// exercicio 1
+
+// const acc = function () {
+//   let valueOne = prompt("insert min: ");
+//   let valueTwo = prompt("insert max: ");
+//   let min = valueOne < valueTwo ? +valueOne : +valueTwo;
+//   let max = valueOne > valueTwo ? +valueOne : +valueTwo;
+
+//   if (valueOne == valueTwo) {
+//     min = max = valueOne;
+//   }
+
+//   let sum = 0;
+//   for (let i = min; i <= max; i++) {
+//     sum += i;
+//   }
+//   return sum;
+// };
+
+// console.log(acc());
+
+// exercicio 2
