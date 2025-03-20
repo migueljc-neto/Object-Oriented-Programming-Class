54b7957a69f96584cf38ba064d6131001dc5c513
migueljnetoux <migueljneto.ux@gmail.com>
2025-03-20 16:40:24 +0000

    Ficha01_finished

--- Ficha01_Estruturas_Condicionais/script.js
+++ Ficha01_Estruturas_Condicionais/script.js
@@ -21,18 +21,49 @@
 
 // exercicio 2
 
-let valid = false;
-let intArray = [];
-while (valid === false) {
-  let int = Number(prompt("Insert a number:"));
-  intArray.push(int);
+// let valid = false;
+// let intArray = [];
+// while (valid === false) {
+//   let int = Number(prompt("Insert a number:"));
+//   intArray.push(int);
 
-  if (int == 0) {
-    valid = true;
-    const intArrayTrim = intArray.slice(-1);
-    console.log(intArrayTrim);
+//   if (int == 0) {
+//     valid = true;
+//     intArray.pop();
+//     console.log(intArray);
 
-    console.log(Math.max(...intArrayTrim));
-    console.log(Math.min(...intArrayTrim));
-  }
-}
+//     console.log(Math.max(...intArray), Math.min(...intArray));
+//   }
+// }
+
+// exercicio 3
+
+// TMB (Masculino) = 10 X peso (kg) + 6.25 X altura (cm) - 5 x idade
+// (anos) + 5
+// • TMB (Feminino) = 10 X peso (kg) + 6.25 X altura (cm) - 5 X idade
+// (anos) - 161
+
+// const calcTMB = function (gender, altCm, pesoKg, idadeAnos) {
+//   let k = gender == "m" ? 5 : -161;
+
+//   return 10 * pesoKg + 6.25 * altCm - 5 * idadeAnos + k;
+// };
+
+// console.log(calcTMB("m", 1, 1, 1));
+// console.log(calcTMB("f", 1, 1, 1));
+
+// exercicio 4
+
+// const numeroPerfeito = function (num) {
+//   let divs = [];
+//   for (i = num - 1; i >= 1; i--) {
+//     if (num % i == 0) divs.push(i);
+//   }
+
+//   return divs.reduce((acc, curr) => acc + curr, 0) == num;
+// };
+
+// console.log(numeroPerfeito(6));
+// console.log(numeroPerfeito(28));
+// console.log(numeroPerfeito(496));
+// console.log(numeroPerfeito(8128));
