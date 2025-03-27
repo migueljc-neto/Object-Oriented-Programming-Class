b333ba85be2e4b5c48d61146be7a967f53d191cd
migueljnetoux <migueljneto.ux@gmail.com>
2025-03-27 21:28:57 +0000

    Ficha05_reclear

--- Ficha05_DOM/script.js
+++ Ficha05_DOM/script.js
@@ -1,34 +1,20 @@
-// button
-const submitButton = document.querySelector(".submit-button");
+// elements
+// înputs
+const nameInput = document.querySelector("#nameInput");
+const yearInput = document.querySelector("#yearInput");
+const genreInput = document.querySelector("#genreInput");
+const platformInput = document.querySelector("#platformInput");
+const ratingInput = document.querySelector("#ratingInput");
 
-// table
-const table = document.querySelector("#table");
+let lGames = [];
 
-const addGame = submitButton.addEventListener("click", function (e) {
+const addGame = function (e) {
   e.preventDefault();
-  const game = {
-    name: document.querySelector(".name-input").value,
-    date: document.querySelector(".date-input").value,
-    genre: document.querySelector(".genre-input").value,
-    platform: document.querySelector(".platform-input").value,
-    rate: document.querySelector(".rate-input").value,
+  let game = {
+    name: nameInput.value,
+    year: yearInput.value,
+    genre: genreInput.value,
   };
 
-  let row = table.insertRow(1);
-
-  let nameCell = row.insertCell(0);
-  let dateCell = row.insertCell(1);
-  let genreCell = row.insertCell(2);
-  let platformCell = row.insertCell(3);
-  let rateCell = row.insertCell(4);
-  let deleteCell = row.insertCell(5);
-
-  nameCell.innerHTML = game.name;
-  dateCell.innerHTML = game.date;
-  genreCell.innerHTML = game.genre;
-  platformCell.innerHTML = game.platform;
-  rateCell.innerHTML = game.rate;
-  deleteCell.innerHTML = `<button onclick="deleteFunc()">Delete</button>`;
-
-  const deleteFunc = function () {};
-});
+  console.log(game);
+};
